1816c448357e0b6e8699d330fc5a709e6c641f20
Gavino, Czeann Kirbie Santera <ckggithub@gmail.com>
2026-04-05 19:36:41 +0800

    Added Deploy Configurations

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/10-React-Quiz", // this is where you code exist in github
 });
